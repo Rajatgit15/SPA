432b9da8c624a152f5868603519cb6ba5da733bc
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-04-17 17:22:43 +0530

    Updates

---
+++ precache-manifest.fefa651de37d208752b49860d768ec85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d1b157b6c050365b526c837b39e549",
+    "url": "/SPA/index.html"
+  },
+  {
+    "revision": "3e18aea842e4d0f77259",
+    "url": "/SPA/static/css/main.fb0316b9.chunk.css"
+  },
+  {
+    "revision": "7f22acf8ea8a1a37b289",
+    "url": "/SPA/static/js/2.6f41c62b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/SPA/static/js/2.6f41c62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e18aea842e4d0f77259",
+    "url": "/SPA/static/js/main.eda91d84.chunk.js"
+  },
+  {
+    "revision": "055c548390668134262f",
+    "url": "/SPA/static/js/runtime-main.7787c859.js"
+  }
+]);
